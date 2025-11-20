313eda40c91cce48190e3969669e86152ddb4785
Valerio Vaccaro <valerio@blockstream.com>
2025-11-20 16:01:50 +0100

    add jade v2 dev board

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ connectButton.onclick = async () => {
     {address: '0x10000', fileName: 'jade.bin', progressBar: jadeprogressBar},
   ];
 
-  if (diymodelsel.value.includes("s3")) {
+  if ((diymodelsel.value.includes("s3")) || (diymodelsel.value.includes("jade_v2"))) {
       addressesAndFiles = [
         {address: '0x0', fileName: 'bootloader.bin', progressBar: btprogressBar},
         {address: '0x8000', fileName: 'partition-table.bin', progressBar: ptprogressBar},
